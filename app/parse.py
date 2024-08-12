4b724e82b612777af6a5cc9757e81c085082e8f4
Vladyslav Shepilov <shepsterya@gmail.com>
2024-08-12 11:17:30 +0300

    Done

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,11 @@
 from dataclasses import dataclass
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+BASE_URL = "https://quotes.toscrape.com/"
+SELECTOR_NEXT_NAV = "li.next > a"
+SELECTOR_QUOTE_BLOCK = "div.quote"
 
 
 @dataclass
@@ -8,8 +15,54 @@ class Quote:
     tags: list[str]
 
 
+def is_next_page(soup: BeautifulSoup) -> bool:
+    next_page = soup.select_one(SELECTOR_NEXT_NAV)
+    return next_page is not None
+
+
+def retrieve_page_soup(page_num: int) -> BeautifulSoup:
+    page_url = BASE_URL + f"page/{page_num}/"
+    response = requests.get(page_url).content
+    soup = BeautifulSoup(response, "html.parser")
+    return soup
+
+
+def parse_single_quote(quote_soup: BeautifulSoup) -> Quote:
+    text = quote_soup.select_one("span.text").text
+    author = quote_soup.select_one("small.author").text
+    tags = [
+        tag.get_text(strip=True)
+        for tag in quote_soup.select("div.tags a.tag")
+    ]
+    return Quote(text=text, author=author, tags=tags)
+
+
+def parse_page(page_soup: BeautifulSoup, result_arr: list) -> list[Quote]:
+    for quote in page_soup.select(SELECTOR_QUOTE_BLOCK):
+        new_quote = parse_single_quote(quote)
+        result_arr.append(new_quote)
+    return result_arr
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    result_arr = []
+    page_num = 1
+
+    # Retrieve and parse the first page
+    page_soup = retrieve_page_soup(page_num)
+    parse_page(page_soup=page_soup, result_arr=result_arr)
+
+    # Continue scraping while there is a next page
+    while is_next_page(page_soup):
+        page_num += 1
+        page_soup = retrieve_page_soup(page_num)
+        parse_page(page_soup, result_arr)
+
+    with open(output_csv_path, "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Text", "Author", "Tags"])
+        for quote in result_arr:
+            writer.writerow([quote.text, quote.author, ", ".join(quote.tags)])
 
 
 if __name__ == "__main__":
